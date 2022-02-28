965cbdd1b345839adf3462922fbeb9bed05f3510
bruce_milliken <brucemilliken98@gmail.com>
2022-02-28 17:01:33 +0000

    solution 3 for change two dimensional array kata

--- kata-7/codewars-change-two-dimensional-array-7.js
+++ kata-7/codewars-change-two-dimensional-array-7.js
@@ -1,4 +1,6 @@
-// Function receive a two-dimensional square array of random integers. On the main diagonal, all the negative integers must be changed to 0, while the others must be changed to 1 (Note: 0 is considered non-negative, here).
+// Function receive a two-dimensional square array of random integers. On the main diagonal,
+// all the negative integers must be changed to 0, while the others must be changed to 1 
+//(Note: 0 is considered non-negative, here).
 
 // (You can mutate the input if you want, but it is a better practice to not mutate the input)
 
@@ -45,10 +47,15 @@ function matrixTwo(array) {
     return array;
 }
    
-console.log(matrixTwo([
-    [-1,  4, -5, -9,  3 ],
-    [ 6, -4, -7,  4, -5 ],
-    [ 3,  5,  0, -9, -1 ],
-    [ 1,  5, -7, -8, -9 ],
-    [-3,  2,  1, -5,  6 ]
-  ]))
+console.log(matrixTwo([[-1,  4, -5, -9,  3 ],[ 6, -4, -7,  4, -5 ],[ 3,  5,  0, -9, -1 ],[ 1,  5, -7, -8, -9 ],[-3,  2,  1, -5,  6 ]]))
+
+  function matrixThree(array) {
+      for (let i=0; i<array.length; i++){
+          if(array[i][i] < 0)
+          array[i][i] = 0
+          else
+          array[i][i] = 1
+      }
+      return array;
+  }
+  console.log(matrixThree([[-1,  4, -5, -9,  3 ],[ 6, -4, -7,  4, -5 ],[ 3,  5,  0, -9, -1 ],[ 1,  5, -7, -8, -9 ],[-3,  2,  1, -5,  6 ]]))
