e044e5fa286cd95e08927049c58bb1ce4fac8167
bruce_milliken <brucemilliken98@gmail.com>
2022-02-28 16:55:38 +0000

    change two dimensional array solution 2

--- kata-7/codewars-change-two-dimensional-array-7.js
+++ kata-7/codewars-change-two-dimensional-array-7.js
@@ -37,3 +37,18 @@ console.log(matrix([
       [ 1,  5, -7, -8, -9 ],
       [-3,  2,  1, -5,  6 ]
     ]))
+
+function matrixTwo(array) {
+    for (let i=0; i<array.length; i++) {
+        array[i][i] = array[i][i] < 0 ? 0 : 1
+    }
+    return array;
+}
+   
+console.log(matrixTwo([
+    [-1,  4, -5, -9,  3 ],
+    [ 6, -4, -7,  4, -5 ],
+    [ 3,  5,  0, -9, -1 ],
+    [ 1,  5, -7, -8, -9 ],
+    [-3,  2,  1, -5,  6 ]
+  ]))
