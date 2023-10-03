941f1aa05581445f8d555446ca8f8b51be7d9f1d
Bruce Milliken <84877032+brucem98@users.noreply.github.com>
2023-10-03 14:57:47 +0100

    Remove String Spaces

---
+++ kata-8/remove-string-spaces.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/57eae20f5500ad98e50002c5/train/javascript
+
+function noSpace(x) {
+  return x.replace(/\s+/g, "");
+}
+
+console.log(noSpace("8 j 8   mBliB8g  imjB8B8  jl  B"));
