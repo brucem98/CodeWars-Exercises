6e692f99425430faba6d3f7cb6bfe54857368795
Bruce Milliken <84877032+brucem98@users.noreply.github.com>
2023-10-10 11:50:59 +0100

    Reduce My Fraction

---
+++ kata-7/reduce-my-fraction.js
@@ -0,0 +1,19 @@
+// https://www.codewars.com/kata/576400f2f716ca816d001614/train/javascript
+
+// Reduce the numerator and denominator to their simplest form using common denominators
+
+function reduce(fraction) {
+  function gcd(a, b) {
+    return b === 0 ? a : gcd(b, a % b); // recursion, checks if b is 0 first, if it is returs a
+  }
+
+  const [numerator, denominator] = fraction;
+  const divisor = gcd(numerator, denominator);
+
+  return [numerator / divisor, denominator / divisor];
+}
+
+console.log(
+  reduce([45, 120]), // [3,8];
+  reduce([80, 120]) // [2, 3]
+);
