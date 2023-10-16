9af9b53cc3437bda2b85ff2b648da29fd15ab83f
Bruce M <84877032+brucem98@users.noreply.github.com>
2023-10-16 22:30:19 +0100

    Break camelCase

---
+++ kata-6/break-camelCase.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/5208f99aee097e6552000148/train/javascript
+
+function solution(string) {
+    return string.replace(/([A-Z])/g, ' $1');
+}
+  
+console.log(solution("hungryBigBear"))
