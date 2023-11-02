cc8ecb41cc27d1500a9ca7762bd007e95ebd98f5
Bruce M <84877032+brucem98@users.noreply.github.com>
2023-11-02 23:53:23 +0000

    Regex Validate PIN code

---
+++ kata-7/regex-validate-PIN-code.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/55f8a9c06c018a0d6e000132/train/javascript
+
+function validatePIN(pin) {
+    return /^(\d{4}|\d{6})$/.test(pin)
+}
+
+console.log(validatePIN('493201'), validatePIN('3740'), validatePIN('386'))
+
+
